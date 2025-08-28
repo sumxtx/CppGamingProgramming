6da77a41263a600e58498d748127937ee46909c9
sumxtx <137871537+sumxtx@users.noreply.github.com>
2025-08-28 19:11:49 +0200

    Bee and Clouds are dynamic. The code still dirty and monolithic tho. Fixing that later

--- Timber.cpp
+++ Timber.cpp
@@ -1,20 +1,85 @@
 #include <iostream>
+#include <string>
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
 
 int main()
 {
-  VideoMode vm(1920, 1080);
+  /* Set the application configurations, Name, Window Size, Window Mode */
+  std::string app_name = "Timber";
+  int win_size_x = 1920;
+  int win_size_y = 1080;
+  VideoMode vm(win_size_x, win_size_y);
 
-  RenderWindow window(vm, "Timber", Style::Fullscreen);
+  RenderWindow window(vm, app_name, Style::Fullscreen);
   
-  Texture textureBackground;
-  textureBackground.loadFromFile("graphics/background.png");
+  /* Create the Texture background set Texture to an Sprite */
+  Texture textureBG;
+  std::string backgroundTexturePath = "graphics/background.png";
+  textureBG.loadFromFile(backgroundTexturePath);
   
-  Sprite spriteBackground;
+  Sprite spriteBG;
+  spriteBG.setTexture(textureBG);
 
-  spriteBackground.setTexture(textureBackground);
+  /* Create the Texture tree set the Texture to an Sprite and set its position on the window*/
+
+  int tree_position_x = 810;
+  int tree_position_y = 0;
+  std::string textureTreePath = "graphics/tree.png";
+  Texture textureTree;
+  textureTree.loadFromFile(textureTreePath);
+
+  Sprite spriteTree;
+  spriteTree.setTexture(textureTree);
+  spriteTree.setPosition(tree_position_x, tree_position_y);
+
+  /* Create the Texture bee and set the Texture to an Sprite and set its position on the window */
+
+  int bee_position_x = 0;
+  int bee_position_y = 800;
+  std::string textureBeePath = "graphics/bee.png";
+  Texture textureBee;
+  textureBee.loadFromFile(textureBeePath);
+
+  Sprite spriteBee;
+  spriteBee.setTexture(textureBee);
+  spriteBee.setPosition(bee_position_x, bee_position_y);
+
+  bool beeActive = false;
+  float beeSpeed = 0.0f;
+
+  /* Create the Texture cloud and cloud Sprites */
+  std::string textureCloudPath = "graphics/cloud.png";
+
+  Texture textureCloud;
+  textureCloud.loadFromFile(textureCloudPath);
+
+  Sprite spriteCloud1;
+  Sprite spriteCloud2;
+  Sprite spriteCloud3;
+
+  spriteCloud1.setTexture(textureCloud);
+  spriteCloud2.setTexture(textureCloud);
+  spriteCloud3.setTexture(textureCloud);
+
+  spriteCloud1.setPosition(0,0);
+  spriteCloud2.setPosition(0,250);
+  spriteCloud3.setPosition(0,500);
+
+  bool cloud1Active = false;
+  bool cloud2Active = false;
+  bool cloud3Active = false;
+
+  float cloud1Speed = 0.0f;
+  float cloud2Speed = 0.0f;
+  float cloud3Speed = 0.0f;
+
+
+  Clock clock;
+
+  bool paused = true;
+
 
   while(window.isOpen())
   {
@@ -23,9 +88,103 @@ int main()
       window.close();
     }
 
+    if(Keyboard::isKeyPressed(Keyboard::Return))
+    {
+      paused = false;
+    }
+
+    Time dt = clock.restart();
+
+    if(!beeActive)
+    {
+
+      srand((int)time(0));
+      beeSpeed = (rand() % 200) + 200;
+
+      srand((int)time(0) * 10);
+      float height = (rand() % 500) + 500;
+      spriteBee.setPosition(2000, height);
+      beeActive = true;
+    }
+    else
+    {
+      spriteBee.setPosition(
+          spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()),
+          spriteBee.getPosition().y );
+      if (spriteBee.getPosition().x < -100)
+      {
+        beeActive = false;
+      }
+    }
+
+    if(!cloud1Active)
+    {
+      srand((int)time(0) * 10);
+      cloud1Speed = (rand() % 200);
+      srand((int)time(0) * 10);
+      float height = (rand() % 150);
+      spriteCloud1.setPosition(-200, height);
+      cloud1Active = true;
+    }
+    else
+    {
+      spriteCloud1.setPosition(
+          spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()),
+          spriteCloud1.getPosition().y);
+      if (spriteCloud1.getPosition().x > 1920)
+      {
+        cloud1Active = false;
+      }
+    }
+
+    if(!cloud2Active)
+    {
+      srand((int)time(0) * 20);
+      cloud2Speed = (rand() % 200);
+      srand((int)time(0) * 20);
+      float height = (rand() % 150);
+      spriteCloud2.setPosition(-200, height);
+      cloud2Active = true;
+    }
+    else
+    {
+      spriteCloud2.setPosition(
+          spriteCloud2.getPosition().x + (cloud2Speed * dt.asSeconds()),
+          spriteCloud2.getPosition().y);
+      if (spriteCloud2.getPosition().x > 1920)
+      {
+        cloud2Active = false;
+      }
+    }
+
+    if(!cloud3Active)
+    {
+      srand((int)time(0) * 30);
+      cloud3Speed = (rand() % 200);
+      srand((int)time(0) * 30);
+      float height = (rand() % 150);
+      spriteCloud3.setPosition(-200, height);
+      cloud3Active = true;
+    }
+    else
+    {
+      spriteCloud3.setPosition(
+          spriteCloud3.getPosition().x + (cloud3Speed * dt.asSeconds()),
+          spriteCloud3.getPosition().y);
+      if (spriteCloud3.getPosition().x > 1920)
+      {
+        cloud3Active = false;
+      }
+    }
+
     window.clear();
 
-    window.draw(spriteBackground);
+    window.draw(spriteBG);
+    window.draw(spriteCloud1);
+    window.draw(spriteCloud2);
+    window.draw(spriteCloud3);
+    window.draw(spriteTree);
+    window.draw(spriteBee);
 
     window.display();
   }
