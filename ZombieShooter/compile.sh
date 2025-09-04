6a23a5051347b1e4231cf3ab37233e01d0c3a677
sumxtx <137871537+sumxtx@users.noreply.github.com>
2025-09-04 23:05:12 +0200

    Zombie Shooter Game. View. Start/Stop Game logic. Player movement effect. Map and borders generation

---
+++ ZombieShooter/compile.sh
@@ -0,0 +1,18 @@
+## TODO Makefile
+
+## Library -> https://www.sfml-dev.org/download/sfml/2.6.0/
+
+# 
+# if the extracted Library is one directory above the src files
+#
+
+
+## libFLAC.s0.12 required by SFML play sounds, otherwise won't compile ->https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.4.0.tar.xz
+## Compile it but doesn't `sudo make install`,
+## instead copy the libFLAC.so.12.0.0 as ../../SFML-2.6.0/libFLAC.so.12
+
+# 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../SFML-2.6.0/lib
+#
+g++ -c Player.cpp CreateBackground.cpp ZombieArena.cpp -I../../SFML-2.6.0/include -I.
+g++ ZombieArena.o Player.o CreateBackground.o -o ZombieArena -L../../SFML-2.6.0/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio ../../SFML-2.6.0/lib/libFLAC.so.12
